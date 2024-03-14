c315fe66e09f8b14068db704c1ca34645643605e
sotech117 <michael_foiani@brown.edu>
2024-03-14 17:21:28 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -12,6 +12,11 @@ function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
 
+  // POST REQUEST
+  // URL: https://cab.brown.edu/api/?page=fose&route=search&is_ind_study=N&is_canc=N
+  // URL PARAMS: page=fose & route = promoted
+  // 
+
   // make a state variable to hold the data
   const [data, setData] = useState(bakeryData);
   const [cart, setCart] = useState([]);
@@ -19,12 +24,19 @@ function App() {
 
   // run when this component is first loaded
   const loadData = () => {
-    setData(bakeryData);
+    setData([...bakeryData]);
   }
 
+  
   useEffect(() => {
+    console.log('component loaded', data.length)
     loadData();
-  }, [])
+  }, []) // run once when the component is first loaded if []
+
+  useEffect(() => {
+    console.log('cart changed: ', cart.length)
+    console.log('data: ', data.length)
+  }, [cart, data]) // if there's a value (or more) in here, run this function every time one of those values changes
 
   const addToCart = (price) => {
     console.log('adding to cart:', price)
@@ -34,35 +46,44 @@ function App() {
         .filter(price => price < 5))
   }
 
-  const buildElements = () => {
-    const jsxlist = bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+  const bakeryItemsJSX = data.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
       <p onClick={(e) => {addToCart(item.price)}}>Bakery Item {index}, {item.description}, { item.price}</p> // replace with BakeryItem component
-    ))
+  ))
 
-    return jsxlist;
-  }
 
-  const showCart = () => {
-    if (cart.length === 0) {
-      console.log('cart is empty')
-      return <p>Cart is empty</p>
-    }
+  const cartJSX = cart.length === 0 ? <p>Cart is empty</p>
+    : cart.map((price, index) => <p key={index} >$ {price}</p>)
+  
+  // const showCart = () => {
+  //   if (cart.length === 0) {
+  //     console.log('cart is empty')
+  //     return <p>Cart is empty</p>
+  //   }
 
-    const jsxlist = cart.map((price, index) => {
-      return <p key={index} >$ {price}</p>
-    })
+  //   const jsxlist = cart.map((price, index) => {
+  //     return <p key={index} >$ {price}</p>
+  //   })
 
-    return jsxlist;
-  }
+  //   return jsxlist;
+  // }
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
-      {showCart()}
+      {cartJSX}
 
-      {buildElements()}
+      {bakeryItemsJSX}
 
+      <button onClick={() => {
+        console.log('filtering data')
+        setData(prev_data => prev_data.filter((item, index) => index % 2 === 0))
+      }}>filter</button>
+
+      <button onClick={() => {
+        console.log('restoring data')
+        setData(bakeryData) // original data
+      }}>clear</button>
 
       <div>
         <h2>Cart</h2>
